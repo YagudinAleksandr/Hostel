0c46cc8d8e48f0db0fcae45ae82ccbc392e30217
Александр <@USER>
2023-06-07 23:27:22 +0300

    Добавьте файлы проекта.

---
+++ UI/Hostel.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Hostel.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ UI/Hostel.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Hostel.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
