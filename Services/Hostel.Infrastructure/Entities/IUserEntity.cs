99ab64a9ecb662ec26ab9058316915807f70008b
Александр <@USER>
2023-06-11 19:19:20 +0300

    Сущность пользователя

---
+++ Services/Hostel.Infrastructure/Entities/IUserEntity.cs
@@ -0,0 +1,12 @@
+﻿using Hostel.Infrastructure.Entities.Base;
+
+namespace Hostel.Infrastructure.Entities
+{
+    public interface IUserEntity : IBaseEntity
+    {
+        string Fullname { get; set; }
+        bool IsAdmin { get; set; }
+        bool IsActive { get; set; }
+        string ProfileImg { get; set; }
+    }
+}
