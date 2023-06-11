9f30a96c89b9af82db1a29e2720a812acc7abb64
Александр <@USER>
2023-06-11 19:19:07 +0300

    Базовый интерфейс сущности

--- Services/Hostel.Infrastructure/Entities/Base/IBaseEntity.cs
+++ Services/Hostel.Infrastructure/Entities/Base/IBaseEntity.cs
@@ -2,9 +2,8 @@
 
 namespace Hostel.Infrastructure.Entities.Base
 {
-    public interface IBaseEntity<T>
+    public interface IBaseEntity
     {
-        T ID { get; set; }
         DateTimeOffset CreatedAt { get; set; }
         DateTimeOffset UpdatedAt { get; set; }
     }
