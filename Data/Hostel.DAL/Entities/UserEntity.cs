887b7770ac5953aa966ab685646b219fca41453e
Александр <@USER>
2023-06-11 19:19:52 +0300

    Сущность пользователя

---
+++ Data/Hostel.DAL/Entities/UserEntity.cs
@@ -0,0 +1,16 @@
+﻿using Hostel.Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Hostel.DAL.Entities
+{
+    public class UserEntity: IdentityUser, IUserEntity
+    {
+        public string Fullname { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsActive { get; set; }
+        public string ProfileImg { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
