1108246907bd545d85dbfeb47d43d7c1e88073f9
Александр <@USER>
2023-06-11 17:12:20 +0300

    Подключение библиотек

---
+++ Services/Hostel.Infrastructure/Entities/Base/IBaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Hostel.Infrastructure.Entities.Base
+{
+    public interface IBaseEntity<T>
+    {
+        T ID { get; set; }
+        DateTimeOffset CreatedAt { get; set; }
+        DateTimeOffset UpdatedAt { get; set; }
+    }
+}
