c2ce54ab67bb061365f99cdbea6a3b0b6bc3e352
Александр <@USER>
2023-06-11 19:20:09 +0300

    Первые настройки контекста БД

---
+++ Data/Hostel.DAL/Context/DataDB.cs
@@ -0,0 +1,26 @@
+﻿using Hostel.DAL.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostel.DAL.Context
+{
+    public class DataDB : IdentityDbContext<UserEntity>
+    {
+        public DataDB(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
