28ca44411d97f2d4bcde347ba51c6db8a9b76fca
Александр <@USER>
2023-06-12 16:40:03 +0300

    Класс хэширования пароля

---
+++ Domain/Hostel.Domain.Security/PasswordIdentification.cs
@@ -0,0 +1,29 @@
+﻿namespace Hostel.Domain.Security
+{
+    /// <summary>
+    /// Идентификация и создание пароля
+    /// </summary>
+    public static class PasswordIdentification
+    {
+        /// <summary>
+        /// Получение хэша пароля
+        /// </summary>
+        /// <param name="password">Пароль в открытом виде</param>
+        /// <returns>Хэш пароля</returns>
+        public static string GetHashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        /// <summary>
+        /// Проверка валидности пароля
+        /// </summary>
+        /// <param name="requestPassword">Запрашиваемое значение пароля</param>
+        /// <param name="modelPassword">Хэш модели пароля</param>
+        /// <returns>True - совпадает хэш, False - не совпадает</returns>
+        public static bool GetValidPassword(string requestPassword, string modelPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(requestPassword, modelPassword);
+        }
+    }
+}
