4ac9e8f74d37dd3b8ade9cdda1496c492c56c348
Александр <@USER>
2023-06-14 22:12:12 +0300

    Модели для авторизации пользователя

---
+++ Domain/Hostel.Domain.DTO/UsersDTOs/UserLoginRequestDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hostel.Domain.DTO.UsersDTOs
+{
+    /// <summary>
+    /// Класс пользователя на запрос авторизации
+    /// </summary>
+    public class UserLoginRequestDTO
+    {
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        [Required(ErrorMessage ="Поле E-mail должно быть заполнено")]
+        [EmailAddress(ErrorMessage = "Неверный стандарт записи почты")]
+        public string Username { get; set; }
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        [Required(ErrorMessage = "Поле Пароль должно быть заполнено")]
+        public string Password { get; set; }
+        /// <summary>
+        /// Является ли запрос с сервера
+        /// </summary>
+        public bool IsServer { get; set; }
+    }
+}
---
+++ Domain/Hostel.Domain.DTO/UsersDTOs/UserLoginResponseDTO.cs
@@ -0,0 +1,26 @@
+﻿namespace Hostel.Domain.DTO.UsersDTOs
+{
+    public class UserLoginResponseDTO
+    {
+        /// <summary>
+        /// Выполнена ли авторизация
+        /// </summary>
+        public bool IsAuthSuccessful { get; set; }
+        /// <summary>
+        /// Ошибки при авторизации
+        /// </summary>
+        public string ErrorMessage { get; set; }
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string Username { get; set; }
+        /// <summary>
+        /// Токен
+        /// </summary>
+        public string Token { get; set; }
+        /// <summary>
+        /// Сессия
+        /// </summary>
+        public string SessionID { get; set; }
+    }
+}
