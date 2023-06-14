3b8a87b67de6843dea3cda919523265d31c9be28
Александр <@USER>
2023-06-14 22:11:42 +0300

    Внесение пользователя в БД

--- Data/Hostel.DAL/Context/DataDB.cs
+++ Data/Hostel.DAL/Context/DataDB.cs
@@ -1,6 +1,8 @@
 ﻿using Hostel.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Hostel.DAL.Context
 {
@@ -17,6 +19,25 @@ namespace Hostel.DAL.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            var hasher = new PasswordHasher<UserEntity>();
+
+            modelBuilder.Entity<UserEntity>()
+                .HasData(new UserEntity
+                {
+                    Id = "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                    UserName = "admin@supply.ru",
+                    Email = "admin@supply.ru",
+                    NormalizedEmail = "admin@supply.ru",
+                    IsActive = true,
+                    IsAdmin = true,
+                    NormalizedUserName = "Администратор",
+                    Fullname = "Администратор",
+                    CreatedAt = DateTimeOffset.UtcNow,
+                    UpdatedAt = DateTimeOffset.UtcNow,
+                    PasswordHash = hasher.HashPassword(null, "password651"),
+                    EmailConfirmed = true,
+                });
         }
 
 
