4717834e742b12c14e49a8488844faea6980c951
Александр <@USER>
2023-06-17 14:49:21 +0300

    Механизм постраничного отображения

---
+++ Services/Hostel.Infrastructure/Pagination/Entities/PagingResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Hostel.Infrastructure.Pagination.Entities
+{
+    /// <summary>
+    /// Ответ страницы с постраничным отображением
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PagingResponse<T>
+    {
+        /// <summary>
+        /// Элементы страницы
+        /// </summary>
+        public List<T> Items { get; set; }
+        /// <summary>
+        /// Метаданные страницы
+        /// </summary>
+        public MetaData MetaData { get; set; }
+    }
+}
