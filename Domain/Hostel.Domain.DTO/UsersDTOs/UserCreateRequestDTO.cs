b63d890fbc4ae5d3ee2e9ef1584ad4b3050a9022
Александр <@USER>
2023-06-18 00:23:50 +0300

    Модель DTO для создания пользователя

---
+++ Domain/Hostel.Domain.DTO/UsersDTOs/UserCreateRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hostel.Domain.DTO.UsersDTOs
+{
+    public class UserCreateRequestDTO
+    {
+        public string Fullname { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsActive { get; set; }
+        public string ProfileImg { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+    }
+}
