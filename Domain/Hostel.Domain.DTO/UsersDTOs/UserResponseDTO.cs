f24652076a6a0337252ef7db9a6547a661ec5dee
Александр <@USER>
2023-06-18 00:24:07 +0300

    Модель получения сущности пользователя

---
+++ Domain/Hostel.Domain.DTO/UsersDTOs/UserResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Hostel.Domain.DTO.UsersDTOs
+{
+    public class UserResponseDTO
+    {
+        public string Id { get; set; }
+        public string NormilizedEmail { get; set; }
+        public string UserName { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsAdmin { get; set; }
+        public string NormalizedUserName { get; set; }
+        public string Fullname { get; set; }
+        public string ProfileImg { get; set; }
+    }
+}
