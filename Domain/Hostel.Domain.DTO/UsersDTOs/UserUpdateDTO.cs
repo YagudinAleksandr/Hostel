8c979e7fa6972d47d175633a0dfb3e646cc5cf61
Александр <@USER>
2023-06-18 23:37:56 +0300

    Модель DTO для обновления пользователя

---
+++ Domain/Hostel.Domain.DTO/UsersDTOs/UserUpdateDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Hostel.Domain.DTO.UsersDTOs
+{
+    public class UserUpdateDTO
+    {
+        public string Id { get; set; }
+        public string Fullname { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsActive { get; set; }
+        public string ProfileImg { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+    }
+}
