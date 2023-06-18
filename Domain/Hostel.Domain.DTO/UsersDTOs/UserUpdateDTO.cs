5c5f0eac126cf802d1f796c8b8f449b09b42e9b4
Александр <@USER>
2023-06-19 00:01:16 +0300

    Обновление модели DTO для обновление пользователя

--- Domain/Hostel.Domain.DTO/UsersDTOs/UserUpdateDTO.cs
+++ Domain/Hostel.Domain.DTO/UsersDTOs/UserUpdateDTO.cs
@@ -9,5 +9,6 @@
         public string ProfileImg { get; set; }
         public string Email { get; set; }
         public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
