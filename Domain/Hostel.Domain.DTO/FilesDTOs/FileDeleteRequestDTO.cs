195f038ff2a7552b305243c871bfa2b12407fa58
Александр <@USER>
2023-06-19 19:29:57 +0300

    DTO модели для удаления файлов

---
+++ Domain/Hostel.Domain.DTO/FilesDTOs/FileDeleteRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Hostel.Domain.DTO.FilesDTOs
+{
+    public class FileDeleteRequestDTO
+    {
+        public string Directory { get; set; }
+        public string Filename { get; set; }
+    }
+}
---
+++ Domain/Hostel.Domain.DTO/FilesDTOs/FileDeleteResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Hostel.Domain.DTO.FilesDTOs
+{
+    public class FileDeleteResponseDTO
+    {
+        public bool IsSuccessful { get; set; }
+        public string Errors { get; set; }
+    }
+}
