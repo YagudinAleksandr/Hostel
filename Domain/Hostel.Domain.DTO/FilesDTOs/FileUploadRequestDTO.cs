a0c880c0d366dfa77f61990cdf6a1f68d9ecc094
Александр <@USER>
2023-06-19 19:29:40 +0300

    DTO модели для загрузки файлов

---
+++ Domain/Hostel.Domain.DTO/FilesDTOs/FileUploadRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Hostel.Domain.DTO.FilesDTOs
+{
+    public class FileUploadRequestDTO
+    {
+        public string DocumentType { get; set; }
+        public string Directory { get; set; }
+    }
+}
---
+++ Domain/Hostel.Domain.DTO/FilesDTOs/FileUploadResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Hostel.Domain.DTO.FilesDTOs
+{
+    public class FileUploadResponseDTO
+    {
+        public bool IsSuccesful { get; set; }
+        public string Errors { get; set; }
+        public string FileName { get; set; }
+    }
+}
