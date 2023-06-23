0ccf148ffce63d2dd53c7ed95a08c7bb042f16a7
Александр <@USER>
2023-06-24 00:42:51 +0300

    Добавлено расширение для сервиса HTTP

---
+++ UI/Hostel.BlazorUI/Infrastructure/Extensions/ServiceExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hostel.BlazorUI.Infrastructure.Extensions
+{
+    /// <summary>
+    /// Класс расширения для подключения сервисов HTTP Client
+    /// </summary>
+    public static class ServiceExtension
+    {
+        /// <summary>
+        /// Добавление сервисов работы через API
+        /// </summary>
+        /// <typeparam name="IInterface">Интерфейс репозитория</typeparam>
+        /// <typeparam name="IClient">Клиент реализовывающий интерфейс</typeparam>
+        /// <param name="services">Сервис HTTP</param>
+        /// <param name="address">Адрес обращения к API</param>
+        /// <returns></returns>
+        public static IHttpClientBuilder AddApi<IInterface, IClient>(this IServiceCollection services, string address)
+            where IInterface : class where IClient : class, IInterface => services
+            .AddHttpClient<IInterface, IClient>(
+                (host, client) => client.BaseAddress = new($"{host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress}{address}"));
+    }
+}
