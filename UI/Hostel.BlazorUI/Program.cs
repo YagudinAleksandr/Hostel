95447d2c78a64c03fd227bd04a9bfea797a76a32
Александр <@USER>
2023-06-24 18:33:34 +0300

    Подключение сервисов

--- UI/Hostel.BlazorUI/Program.cs
+++ UI/Hostel.BlazorUI/Program.cs
@@ -1,12 +1,12 @@
+using Hostel.BlazorUI.Infrastructure.Extensions;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Hostel.Infrastructure.Repositories;
+using Hostel.Domain.DTO.UsersDTOs;
+using Hostel.WebAPIClient;
 
 namespace Hostel.BlazorUI
 {
@@ -17,7 +17,15 @@ namespace Hostel.BlazorUI
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            var service = builder.Services;
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            #region Подключение сервисов
+
+            service.AddApi<IWebUsersRepository<UserCreateRequestDTO, UserUpdateDTO, UserResponseDTO>, WebUsersRepository<UserCreateRequestDTO, UserUpdateDTO, UserResponseDTO>>("api/Users/");
+
+            #endregion
 
             await builder.Build().RunAsync();
         }
