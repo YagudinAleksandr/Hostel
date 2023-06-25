b98dc6735f06d8ef736137adad827c974eebff76
Александр <@USER>
2023-06-25 14:01:19 +0300

    Расширение для сервисов

--- UI/Hostel.BlazorUI/Infrastructure/Extensions/ServiceExtension.cs
+++ UI/Hostel.BlazorUI/Infrastructure/Extensions/ServiceExtension.cs
@@ -14,11 +14,12 @@ namespace Hostel.BlazorUI.Infrastructure.Extensions
         /// <typeparam name="IInterface">Интерфейс репозитория</typeparam>
         /// <typeparam name="IClient">Клиент реализовывающий интерфейс</typeparam>
         /// <param name="services">Сервис HTTP</param>
+        /// <param name="baseAddress">Базовый адрес сервиса</param>
         /// <param name="address">Адрес обращения к API</param>
         /// <returns></returns>
-        public static IHttpClientBuilder AddApi<IInterface, IClient>(this IServiceCollection services, string address)
+        public static IHttpClientBuilder AddApi<IInterface, IClient>(this IServiceCollection services,string baseAddress, string address)
             where IInterface : class where IClient : class, IInterface => services
             .AddHttpClient<IInterface, IClient>(
-                (host, client) => client.BaseAddress = new($"{host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress}{address}"));
+                (host, client) => client.BaseAddress = new($"{baseAddress}/{address}"));
     }
 }
