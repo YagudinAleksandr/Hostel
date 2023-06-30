7af87f0025206ebcf23b782ebbe7d6c5f2d6c1dd
Александр <@USER>
2023-06-30 15:01:27 +0300

    Интерфейс работы с файлами

--- Services/Hostel.Infrastructure/Repositories/IWebFilesRepository.cs
+++ Services/Hostel.Infrastructure/Repositories/IWebFilesRepository.cs
@@ -1,21 +1,22 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Hostel.Infrastructure.Repositories
 {
-    public interface IWebFilesRepository<TCreateRequest, TCreateResponse, TDeleteRequest, TDeleteResponse>
-        where TCreateRequest : class where TCreateResponse : class where TDeleteRequest : class where TDeleteResponse : class
+    public interface IWebFilesRepository<TCreateResponse, TDeleteResponse>
+       where TCreateResponse : class where TDeleteResponse : class
     {
         /// <summary>
         /// Загрузка файлов
         /// </summary>
         /// <param name="request">Тело запроса на создание</param>
         /// <returns>Ответ от API</returns>
-        Task<TCreateResponse> Upload(TCreateRequest request);
+        Task<TCreateResponse> Upload(MultipartFormDataContent content);
         /// <summary>
         /// Удаление файла
         /// </summary>
         /// <param name="request">Тело запроса</param>
         /// <returns>Ответ от API</returns>
-        Task<TDeleteResponse> Delete(TDeleteRequest request);
+        Task<TDeleteResponse> Delete(string name);
     }
 }
