fb293d9411d660498f0eaa5b925891e05c446aba
Александр <@USER>
2023-07-01 17:51:02 +0300

    Компонент всплывющих окон

---
+++ UI/Hostel.BlazorUI/Components/ToastNotifyComponent.razor.cs
@@ -0,0 +1,18 @@
+﻿using Blazored.Toast;
+using Microsoft.AspNetCore.Components;
+
+namespace Hostel.BlazorUI.Components
+{
+    public partial class ToastNotifyComponent
+    {
+        [CascadingParameter]
+        private BlazoredToast ToastInstance { get; set; } = default!;
+
+        [Parameter]
+        public string? Title { get; set; }
+        [Parameter]
+        public string? ToastParam { get; set; }
+        [Parameter]
+        public string Type { get; set; }
+    }
+}
