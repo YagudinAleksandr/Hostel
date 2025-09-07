93a8ddf7360498a094bc5e2bf5495697fc1ce5d1
Александр <@DESKTOP-L9V170T>
2025-09-07 14:43:40 +0300

    #6 Токен сброса пароля

---
+++ src/Hostel.SU.Domain/Exceptions/DomainUsedTokenException.cs
@@ -0,0 +1,19 @@
+﻿using Hostel.Shared.Kernel;
+
+namespace Hostel.SU.Domain
+{
+    /// <summary>
+    /// Исключение использованного токена
+    /// </summary>
+    public class DomainUsedTokenException : DomainException
+    {
+        /// <summary>
+        /// Исключение использованного токена
+        /// </summary>
+        public DomainUsedTokenException()
+            : base(ServicesUsersExceptionCodes.ServicesUsersExceptionUsedToken)
+        {
+
+        }
+    }
+}
