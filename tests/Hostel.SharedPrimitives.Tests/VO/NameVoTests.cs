3b7cd0da643b6dc6faf38df25811a754941040e6
a.yagudin <a.yagudin@DEV-01>
2025-09-08 14:49:28 +0300

    #9 Тест на NameVO

---
+++ tests/Hostel.SharedPrimitives.Tests/VO/NameVoTests.cs
@@ -0,0 +1,21 @@
+﻿using Hostel.Domain.Primitives;
+
+namespace Hostel.SharedPrimitives.Tests
+{
+    /// <summary>
+    /// Тесты на VO <see cref="NameVo"/>
+    /// </summary>
+    public class NameVoTests
+    {
+        [Fact(DisplayName = "Формирование названия")]
+        public async Task Should_Create_Name()
+        {
+            string name = "Test name";
+            var nameVo = new NameVo(name);
+
+            Assert.NotNull(nameVo);
+            Assert.Equal(nameVo.Value, name);
+        }
+
+    }
+}
