722c871b27ef999364fcdea4781b2a5be99a17ae
a.yagudin <a.yagudin@DEV-01>
2025-09-08 15:51:03 +0300

    #9 Тесты для FullNameVO (1 часть)

---
+++ tests/Hostel.SharedPrimitives.Tests/VO/FullNameVoTests.cs
@@ -0,0 +1,22 @@
+﻿using Hostel.Domain.Primitives;
+
+namespace Hostel.SharedPrimitives.Tests
+{
+    /// <summary>
+    /// Тесты для Value Object <see cref="FullNameVo"/>
+    /// </summary>
+    public class FullNameVoTests
+    {
+        [Theory(DisplayName = "")]
+        [InlineData("Тест", "Test", "Test")]
+        [InlineData("Тест", "Тест", null)]
+        public async Task Should_Create_Full_Name(string lastname, string firstname, string? patronymic)
+        {
+            var fullname = new FullNameVo(firstname, lastname, patronymic);
+
+            Assert.NotNull(fullname);
+            Assert.Equal(firstname, fullname.FirstName);
+            Assert.Equal(lastname, fullname.LastName);
+        }
+    }
+}
