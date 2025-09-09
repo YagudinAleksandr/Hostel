2e98be444e94b4ecd96d0f65765a1bdc97ef60e0
Александр <@DESKTOP-L9V170T>
2025-09-09 23:58:39 +0300

    #7 Контракты для пользователей

---
+++ Shared/Contracts/Hostel.Users.Contracts/Response/UserResponse.cs
@@ -0,0 +1,45 @@
+﻿namespace Hostel.Users.Contracts.Response
+{
+    /// <summary>
+    /// Пользователь
+    /// </summary>
+    public record UserResponse
+    {
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Firstname { get; set; } = null!;
+
+        /// <summary>
+        /// Отчесвто
+        /// </summary>
+        public string? Patronymic { get; set; }
+
+        /// <summary>
+        /// Тип
+        /// </summary>
+        public string Type { get; set; } = null!;
+
+        /// <summary>
+        /// Статус
+        /// </summary>
+        public string Status { get; set; } = null!;
+
+        /// <summary>
+        /// Адрес жжлектронной почты
+        /// </summary>
+        public string Email { get; set; } = null!;
+
+        /// <summary>
+        /// Дата создания
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Дата изменения
+        /// </summary>
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
