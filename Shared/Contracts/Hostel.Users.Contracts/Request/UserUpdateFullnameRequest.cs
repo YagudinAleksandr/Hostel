147d0581abb607e84846207bdac8ccf590a06884
a.yagudin <a.yagudin@DEV-01>
2025-09-10 16:47:19 +0300

    #7 Команда на изменение ФИО

---
+++ Shared/Contracts/Hostel.Users.Contracts/Request/UserUpdateFullnameRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace Hostel.Users.Contracts.Request
+{
+    /// <summary>
+    /// Запрос на изменение ФИО пользователя
+    /// </summary>
+    public record UserUpdateFullnameRequest
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public Guid Id { get; set; }
+        public string Lastname { get; set; } = null!;
+        public string Firstname { get; set; } = null!;
+        public string? Patronymic { get; set; }
+    }
+}
