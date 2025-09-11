c08c8b56733d0b7aba35eec107917e8d8b4917e8
a.yagudin <a.yagudin@DEV-01>
2025-09-11 17:23:02 +0300

    #9 Добавлены тесты для обновления E-mail

---
+++ tests/Hostel.SUApplication.Tests/Commands/UpdateUserEmailCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Hostel.Shared.Kernel;
+using Hostel.SU.Application;
+using Hostel.SU.Domain;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Hostel.SUApplication.Tests.Commands
+{
+    /// <summary>
+    /// Тесты для обработчика команды <see cref="UpdateUserEmailHandler"/>
+    /// </summary>
+    public class UpdateUserEmailCommandTests
+    {
+        private readonly Mock<IUserRepository> _mockRepo;
+        private readonly Mock<IUnitOfWork> _mockUow;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly Mock<ILogger<UpdateUserEmailHandler>> _mockLogger;
+
+        private readonly UpdateUserEmailHandler _handler;
+
+        public UpdateUserEmailCommandTests()
+        {
+            _mockRepo = new Mock<IUserRepository>();
+            _mockUow = new Mock<IUnitOfWork>();
+            _mockMapper = new Mock<IMapper>();
+            _mockLogger = new Mock<ILogger<UpdateUserEmailHandler>>();
+
+            _handler = new UpdateUserEmailHandler(_mockRepo.Object, _mockUow.Object, _mockMapper.Object, _mockLogger.Object);
+        }
+    }
+}
