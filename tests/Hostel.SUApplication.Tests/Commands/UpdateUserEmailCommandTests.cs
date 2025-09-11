e309f4c1e7e7da446fb8db0fb925fbb1dd057cda
a.yagudin <a.yagudin@DEV-01>
2025-09-11 18:18:39 +0300

    #9 Обновление теста для обновления адреса электронной почты пользователя

--- tests/Hostel.SUApplication.Tests/Commands/UpdateUserEmailCommandTests.cs
+++ tests/Hostel.SUApplication.Tests/Commands/UpdateUserEmailCommandTests.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
+using Hostel.Domain.Primitives;
 using Hostel.Shared.Kernel;
 using Hostel.SU.Application;
 using Hostel.SU.Domain;
+using Hostel.Users.Contracts.Request;
+using Hostel.Users.Contracts.Response;
 using Microsoft.Extensions.Logging;
 using Moq;
 
@@ -27,6 +30,49 @@ namespace Hostel.SUApplication.Tests.Commands
             _mockLogger = new Mock<ILogger<UpdateUserEmailHandler>>();
 
             _handler = new UpdateUserEmailHandler(_mockRepo.Object, _mockUow.Object, _mockMapper.Object, _mockLogger.Object);
+        }
+
+        [Fact(DisplayName = "Удачное обновление адреса электронной почты")]
+        public async Task Handle_ShouldReturnSuccess_WhenUserUpdateEmailSuccessfully()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+
+            var request = new UpdateUserEmailCommand(new UserUpdateEmailRequest()
+            {
+                Id = userId,
+                Email = "test2@example.com"
+            });
+
+            _mockRepo.Setup(r => r.GetByIdAsync(userId, default)).ReturnsAsync(User.Create(
+                new EmailVo("test@example.com"),
+                new FullNameVo("John", "Doe", "Smith"),
+                "hashedPassword",
+                UserTypes.Standart,  // мокаем первый тип
+                UserStatuses.Inactive // мокаем первый статус
+            ));
+
+            _mockRepo.Setup(r => r.IsEmailExists("test@example.com", userId)).Returns(false);
+            _mockMapper.Setup(m => m.Map<UserResponse>(It.IsAny<User>())).Returns(new UserResponse
+            {
+                Id = userId,
+                Email = "test2@example.com",
+                Firstname = "John",
+                Lastname = "Doe",
+                Patronymic = "Smith",
+                Type = "STANDART",
+                Status = "INACTIVE"
+            });
+
+            // Act
+            var result = await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Value);
+            _mockUow.Verify(u => u.BeginTransactionAsync(CancellationToken.None), Times.Once);
+            _mockUow.Verify(u => u.CommitAsync(CancellationToken.None), Times.Once);
+            _mockRepo.Verify(r => r.Update(It.IsAny<User>()), Times.Once);
         }
     }
 }
