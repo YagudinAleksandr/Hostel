9d76ea5f6455f5f3ffccbacb621851f73648a830
Александр <@DESKTOP-L9V170T>
2025-09-13 01:19:56 +0300

    #7 реализация авторизации

---
+++ src/Hostel.SU.Application/Services/AuthService.cs
@@ -0,0 +1,122 @@
+﻿using AutoMapper;
+using Hostel.Shared.Kernel;
+using Hostel.SU.Domain;
+using Hostel.Users.Contracts.Response;
+
+namespace Hostel.SU.Application
+{
+    /// <inheritdoc/>
+    internal class AuthService : IAuthService
+    {
+        #region CTOR
+
+        /// <inheritdoc cref="IUserRepository"/>
+        private readonly IUserRepository _userRepository;
+
+        /// <inheritdoc cref="IRefreshTokenRepository"/>
+        private readonly IRefreshTokenRepository _refreshTokenRepository;
+
+        /// <inheritdoc cref="ITokenGeneratorService"/>
+        private readonly ITokenGeneratorService _tokenGeneratorService;
+
+        /// <inheritdoc cref="IPasswordService"/>
+        private readonly IPasswordService _passwordService;
+
+        /// <inheritdoc cref="IMapper"/>
+        private readonly IMapper _mapper;
+
+        public AuthService(IUserRepository userRepository,
+            IRefreshTokenRepository refreshTokenRepository,
+            ITokenGeneratorService tokenGeneratorService,
+            IPasswordService passwordService,
+            IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _refreshTokenRepository = refreshTokenRepository;
+            _tokenGeneratorService = tokenGeneratorService;
+            _passwordService = passwordService;
+            _mapper = mapper;
+        }
+
+        #endregion
+
+        /// <inheritdoc/>
+        public async Task<bool> IsTokenRevokedAsync(string refreshToken, CancellationToken cancellationToken = default)
+        {
+            var token = await _refreshTokenRepository.GetByTokenAsync(refreshToken);
+            return token == null || !token.IsActive;
+        }
+
+        /// <inheritdoc/>
+        public async Task<UserLoginResponse> LoginAsync(string email, string password, CancellationToken cancellationToken = default)
+        {
+            var user = await _userRepository.GetByEmailAsync(email, cancellationToken)
+                ?? throw new DomainResourceNotFoundException(ServicesUsersEntitiesCodes.ServicesUsersEntityUser,
+                    ServicesUsersFieldCodes.ServicesUsersFieldEmail,
+                    email);
+
+            if (user.Status == UserStatuses.Inactive || user.Status == UserStatuses.Blocked)
+                throw new DomainInactiveUserException();
+
+            if (!_passwordService.ValidateHash(password, user.PasswordHash))
+                throw new DomainResourceNotFoundException(ServicesUsersEntitiesCodes.ServicesUsersEntityUser);
+
+            string accessToken = _tokenGeneratorService.GenerateAccessToken(user.Id, user.Type);
+            string refreshToken = _tokenGeneratorService.GenerateRefreshToken(user.Id);
+
+            var refreshTokenEntity = new RefreshToken(refreshToken,
+                DateTime.UtcNow.AddMinutes(_tokenGeneratorService.RefreshTokenLifetimeMinutes),
+                user.Id);
+
+            await _refreshTokenRepository.AddAsync(refreshTokenEntity, cancellationToken);
+
+            return new UserLoginResponse()
+            {
+                AccessToken = accessToken,
+                RefreshToken = refreshToken,
+                User = _mapper.Map<UserResponse>(user)
+            };
+        }
+
+        /// <inheritdoc/>
+        public async Task<UserLoginResponse> RefreshTokenAsync(string refreshToken, CancellationToken cancellationToken = default)
+        {
+            var storedToken = await _refreshTokenRepository.GetByTokenAsync(refreshToken, cancellationToken);
+
+            if (storedToken == null || !storedToken.IsActive)
+                throw new DomainExpiredTokenException();
+
+            var user = await _userRepository.GetByIdAsync(storedToken.UserId, cancellationToken)
+                ?? throw new DomainResourceNotFoundException(ServicesUsersEntitiesCodes.ServicesUsersEntityUser);
+
+            storedToken.Revoke();
+            await _refreshTokenRepository.UpdateAsync(storedToken, cancellationToken);
+
+            string accessToken = _tokenGeneratorService.GenerateAccessToken(user.Id, user.Type);
+            refreshToken = _tokenGeneratorService.GenerateRefreshToken(user.Id);
+
+            var refreshTokenEntity = new RefreshToken(refreshToken,
+                DateTime.UtcNow.AddMinutes(_tokenGeneratorService.RefreshTokenLifetimeMinutes),
+                user.Id);
+
+            await _refreshTokenRepository.AddAsync(refreshTokenEntity, cancellationToken);
+
+            return new UserLoginResponse()
+            {
+                AccessToken = accessToken,
+                RefreshToken = refreshToken,
+                User = _mapper.Map<UserResponse>(user)
+            };
+        }
+
+        /// <inheritdoc/>
+        public async Task RevokeTokenAsync(string refreshToken, CancellationToken cancellationToken = default)
+        {
+            var token = await _refreshTokenRepository.GetByTokenAsync(refreshToken)
+                ?? throw new DomainResourceNotFoundException(ServicesUsersEntitiesCodes.ServicesUsersEntityRefreshToken);
+
+            token.Revoke();
+            await _refreshTokenRepository.UpdateAsync(token);
+        }
+    }
+}
---
+++ src/Hostel.SU.Domain/Services/ITokenGeneratorService.cs
@@ -0,0 +1,33 @@
+﻿namespace Hostel.SU.Domain
+{
+    /// <summary>
+    /// Сервис генерации токенов
+    /// </summary>
+    public interface ITokenGeneratorService
+    {
+        /// <summary>
+        /// Время жизни токена обновления
+        /// </summary>
+        int RefreshTokenLifetimeMinutes { get; }
+
+        /// <summary>
+        /// Время жизни токена доступа
+        /// </summary>
+        int AccessTokenLifetimeMinutes { get; }
+
+        /// <summary>
+        /// Генерация токена дочтупа
+        /// </summary>
+        /// <param name="userId">Идентификатор пользователя</param>
+        /// <param name="type">Тип пользователя</param>
+        /// <returns>Токен доступа</returns>
+        string GenerateAccessToken(Guid userId, UserType type);
+
+        /// <summary>
+        /// Генерация токена обновления
+        /// </summary>
+        /// <param name="userId">Идентификатор пользователя</param>
+        /// <returns>Токен обновления</returns>
+        string GenerateRefreshToken(Guid userId);
+    }
+}
