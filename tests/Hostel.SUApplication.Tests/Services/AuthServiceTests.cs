f8256fa45742a218b0447527ab7e3ccff750c940
a.yagudin <a.yagudin@DEV-01>
2025-09-17 17:56:35 +0300

    #12 Добавлены тесты на проверку авторизации

--- tests/Hostel.SUApplication.Tests/Services/AuthServiceTests.cs
+++ tests/Hostel.SUApplication.Tests/Services/AuthServiceTests.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using Hostel.Shared.Kernel;
 using Hostel.SU.Application;
 using Hostel.SU.Domain;
+using Hostel.Users.Contracts.Response;
 using Moq;
 
 namespace Hostel.SUApplication.Tests.Services
@@ -29,7 +30,7 @@ namespace Hostel.SUApplication.Tests.Services
                 _mockMapper.Object);
         }
 
-        [Fact(DisplayName ="Проверка что токен не истек и не отозван должна вернуть false")]
+        [Fact(DisplayName = "Проверка что токен не истек и не отозван должна вернуть false")]
         public async Task ShouldReturnFalse_IsTokenRevokedAsync_Success()
         {
             // Arrange
@@ -108,7 +109,7 @@ namespace Hostel.SUApplication.Tests.Services
             await _authService.RevokeTokenAsync(token, It.IsAny<CancellationToken>());
 
             // Assert
-            _mockRefreshTokenRepository.Verify(x=>x.UpdateAsync(refreshToken, It.IsAny<CancellationToken>()), Times.Once);
+            _mockRefreshTokenRepository.Verify(x => x.UpdateAsync(refreshToken, It.IsAny<CancellationToken>()), Times.Once);
         }
 
         [Fact(DisplayName = "Отзыв токена, вернуть исключение")]
@@ -116,15 +117,100 @@ namespace Hostel.SUApplication.Tests.Services
         {
             // Arrange
             string token = "refresh_token";
-            var refreshToken = new RefreshToken(token, DateTime.UtcNow.AddDays(1), Guid.NewGuid());
 
             _mockRefreshTokenRepository.Setup(x => x.GetByTokenAsync(token, It.IsAny<CancellationToken>())).ReturnsAsync(default(RefreshToken));
 
             // Act
-            var act = async() => await _authService.RevokeTokenAsync(token, It.IsAny<CancellationToken>());
+            var act = async () => await _authService.RevokeTokenAsync(token, It.IsAny<CancellationToken>());
 
             // Assert
             await act.Should().ThrowAsync<DomainResourceNotFoundException>();
+        }
+
+        [Fact(DisplayName = "Формирование токена после успешной авторизации должен вернуть удачный ответ")]
+        public async Task ShouldReturnTokenResponse_LoginAsync_Success()
+        {
+            // Arrange
+            string email = "test@test.com";
+            string password = "password";
+            var user = User.Create(new EmailVo(email), new Domain.Primitives.FullNameVo("Тест", "Тест", ""), "password", UserTypes.Standart, UserStatuses.Active);
+
+            _mockUserReposytory.Setup(x => x.GetByEmailAsync(email, It.IsAny<CancellationToken>())).ReturnsAsync(user);
+            _mockPasswordsService.Setup(x => x.ValidateHash(password, It.IsAny<string>())).Returns(true);
+            _mockTokenGeneratorService.Setup(x => x.GenerateAccessToken(user.Id, user.Type)).Returns("access_token");
+            _mockTokenGeneratorService.Setup(x => x.GenerateRefreshToken(user.Id)).Returns("access_token");
+            _mockTokenGeneratorService.Setup(x => x.RefreshTokenLifetimeMinutes).Returns(30);
+            _mockMapper.Setup(m => m.Map<UserResponse>(It.IsAny<User>())).Returns(new UserResponse
+            {
+                Id = user.Id,
+                Email = email,
+                Firstname = "John",
+                Lastname = "Doe",
+                Patronymic = "Smith",
+                Type = "ADMINISTRATOR",
+                Status = "ACTIVE"
+            });
+
+            // Act
+            var result = await _authService.LoginAsync(email, password, It.IsAny<CancellationToken>());
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.User);
+            Assert.Equal(email, result.User.Email);
+            _mockRefreshTokenRepository.Verify(x => x.AddAsync(It.IsAny<RefreshToken>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact(DisplayName = "Формирование токена должно вернуть исключение, если пользователь не найден")]
+        public async Task ShouldReturnException_LoginAsync_WhenUserNotFound()
+        {
+            // Arrange
+            string email = "test@test.com";
+            string password = "password";
+
+            _mockUserReposytory.Setup(x => x.GetByEmailAsync(email, It.IsAny<CancellationToken>())).ReturnsAsync(default(User));
+
+            // Act
+            var result = async () => await _authService.LoginAsync(email, password, It.IsAny<CancellationToken>());
+
+            // Assert
+            await result.Should().ThrowAsync<DomainResourceNotFoundException>();
+        }
+
+        [Fact(DisplayName = "Формирование токена должно вернуть исключение, если пароль не совпал")]
+        public async Task ShouldReturnException_LoginAsync_WhenUserPasswordNotCorrect()
+        {
+            // Arrange
+            string email = "test@test.com";
+            string password = "password";
+            var user = User.Create(new EmailVo(email), new Domain.Primitives.FullNameVo("Тест", "Тест", ""), "password", UserTypes.Standart, UserStatuses.Active);
+
+            _mockUserReposytory.Setup(x => x.GetByEmailAsync(email, It.IsAny<CancellationToken>())).ReturnsAsync(user);
+            _mockPasswordsService.Setup(x => x.ValidateHash(password, It.IsAny<string>())).Returns(false);
+
+            // Act
+            var result = async () => await _authService.LoginAsync(email, password, It.IsAny<CancellationToken>());
+
+            // Assert
+            await result.Should().ThrowAsync<DomainResourceNotFoundException>();
+        }
+
+        [Fact(DisplayName = "Формирование токена должно вернуть исключение, если пользователь не активен")]
+        public async Task ShouldReturnException_LoginAsync_WhenUserStatusNotActive()
+        {
+            // Arrange
+            string email = "test@test.com";
+            string password = "password";
+            var user = User.Create(new EmailVo(email), new Domain.Primitives.FullNameVo("Тест", "Тест", ""), "password", UserTypes.Standart, UserStatuses.Active);
+            user.ChangeStatus(UserStatuses.Inactive);
+
+            _mockUserReposytory.Setup(x => x.GetByEmailAsync(email, It.IsAny<CancellationToken>())).ReturnsAsync(user);
+
+            // Act
+            var result = async () => await _authService.LoginAsync(email, password, It.IsAny<CancellationToken>());
+
+            // Assert
+            await result.Should().ThrowAsync<DomainInactiveUserException>();
         }
     }
 }
